a8ba6eb38bece3cbed9f2055b2b08ee22594fa2d
Tomas Sarquis <tomas.sarquis@wazuh.com>
2022-09-10 14:27:55 -0300

    Intro&Level initial configuration

---
+++ Intro/Intro.gd
@@ -0,0 +1,28 @@
+extends Node
+
+export (String, FILE, "*.tscn") var next_scene: String
+
+onready var index = 0
+var timer
+
+
+func _ready():
+	timer = GlobalTimer.add_timeout(self, "next_msg", 10, true, false)
+
+
+func _input(event):
+	if event.is_pressed():
+		next_msg()
+
+
+func next_msg():
+	index += 1
+	var trans_key = str("INTRO-", index)
+	var trans_val = tr(trans_key)
+	
+	if trans_key == trans_val:
+		GlobalTimer.delete_timeout(timer)
+		Game.emit_signal("ChangeScene",next_scene)
+	else:
+		$Message.text = trans_val
+
