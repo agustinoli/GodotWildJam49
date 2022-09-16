377387df977dce789c7fc7404f4524ebc2b88dc5
agustinoli <aolivaarias@gmail.com>
2022-09-16 00:38:00 -0300

    feat: hud implemented, with some funcionalities for the final scene

--- Level/Level.gd
+++ Level/Level.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 export (String, FILE, "*.tscn") var Next_Scene: String
+var timer
 
 func _ready()->void:
+	timer = GlobalTimer.add_timeout(self,"time_over",30)
+	Hud.set_timer(str(int(timer.get_time_left())))
 	Hud.visible = true
 	PauseMenu.can_show = true	
 	var t_shake 	= GlobalTimer.add_timeout(self,"activate_shake",1,false)
@@ -13,6 +16,9 @@ func _exit_tree()->void:
 	Hud.visible = false
 	PauseMenu.can_show = false
 
+
+func _process(_delta):
+	Hud.set_timer(str(int(timer.get_time_left())))
 
 func activate_shake():
 	GlobalTimer.add_timeout(self,"activate_shake",rand_range(1,10),true)
@@ -28,3 +34,13 @@ func activate_blackout():
 func activate_switch():
 	GlobalTimer.add_timeout(self,"activate_switch",rand_range(1,10),true)
 	print("switch: ",OS.get_time())
+
+
+func time_over():	
+	Game.emit_signal("ChangeScene",select_ending())
+
+
+func select_ending()-> String:
+#	Calculates proper ending given the parameters 
+#	and returns the string to the packedscene
+	return "res://MainMenu/MainMenu.tscn"
