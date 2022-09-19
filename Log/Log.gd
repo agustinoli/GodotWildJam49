d0c5f2200293cfd48d58b4dbff2f78cf8a1f9c39
Tomas Sarquis <42682247+tsarquis88@users.noreply.github.com>
2022-09-18 21:19:46 -0300

    Add background, stars particles and title splash screen (#10)
    * Add stars, backgorund and fix BSOD z index
    
    * Enhace init
    
    * Style's shit
    
    * BG in credits
    
    * Some intro improvements

--- Log/Log.gd
+++ Log/Log.gd
@@ -7,7 +7,6 @@ var timer
 func _ready():
 	self.visible = false
 	timer = GlobalTimer.add_timeout(self, "dehold", 3, false, true)
-	find_node("Title").text = tr("LOG-TITLE")
 	find_node("Exit").text = ""
 
 
@@ -18,12 +17,22 @@ func _input(event):
 		find_node("Exit").text = ""
 
 
+func init():
+	self.get_parent().log_active = true
+	hold = true
+	find_node("Message").text = tr("LOG-INIT")
+	find_node("Title").text = tr("LOG-INIT-TITLE")
+	self.visible = true
+	GlobalTimer.start_timeout(timer)
+
+
 func next():
 	self.get_parent().log_active = true
 	message_index += 1
 	SfxManager.play("Piece"+str(message_index))
 	hold = true
 	find_node("Message").text = tr(str("LOG-", message_index))
+	find_node("Title").text = tr("LOG-TITLE")
 	self.visible = true
 	GlobalTimer.start_timeout(timer)
 
